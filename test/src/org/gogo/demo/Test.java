fa2cc9b7448763c8889d30425eac1e784ad31a07
meilagogo <andyleewei@qq.com>
2015-12-21 18:18:53 +0800

    test提交

--- test/src/org/gogo/demo/Test.java
+++ test/src/org/gogo/demo/Test.java
@@ -4,6 +4,10 @@ import java.util.Date;
 
 public class Test {
 
+	public void test()
+	{
+		System.out.println("test");
+	}
 	
 	public static void main(String[] args) {
 		System.out.println(new Date());
